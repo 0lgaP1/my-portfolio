0c0067a552ef2fa7187a93610993f7e78500534a
0lgaP1 <volhapatapchuk@gmail.com>
2024-02-29 14:20:25 +0100

    Created header and main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/img/main-photo.jpg'
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi there!</span>
-                <h2>I'm Olga Patapchuk</h2>
-                <h1>A Web Developer</h1>
-            </div>
-            <image src="" alt="" />
-        </div>
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-between"}>
+                <StyledText>
+                    <span>Hi there!</span>
+                    <h2>I'm Olga Patapchuk</h2>
+                    <h1>A Web Developer</h1>
+                </StyledText>
+                <Photo src={photo} alt="Main photo" />
+            </FlexWrapper>
+        </StyledMain>
     );
 };
 
+const StyledMain = styled.div`
+    min-height: 90vh;
+    background-color: rgba(129, 110, 162, 0.95);
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+const StyledText = styled.div`
+
+`
