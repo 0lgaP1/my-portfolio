38fa7ec022bd2004208968eeebf752947e4259e0
0lgaP1 <volhapatapchuk@gmail.com>
2024-03-05 17:01:57 +0100

    Added contact section

---
+++ src/layout/sections/contacts/Contacts.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {SectionTitle} from "../../../components/SectionTitle";
+import styled from "styled-components";
+import {Button} from "../../../components/Button";
+
+
+export const Contacts = () => {
+    return (
+        <StyledContacts>
+            <SectionTitle>Contact</SectionTitle>
+            <StyledForm>
+                <Field placeholder={"Your name"}/>
+                <Field placeholder={"Enter a subject"}/>
+                <Field placeholder="Enter your message" as={"textarea"} />
+                <Button type={"submit"}>Send your message</Button>
+            </StyledForm>
+
+        </StyledContacts>
+    );
+};
+
+const StyledContacts = styled.section`
+    background-color: rgba(32, 56, 238, 0.55);
+    min-height: 50vh;
+`
+
+const StyledForm = styled.form`
+    max-width: 500px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin: 0 auto;
+`
+
+const Field = styled.input`
+
+`
