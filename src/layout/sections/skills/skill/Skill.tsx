b873d3978a788578386a0c5090cf83ff01d5cb23
0lgaP1 <volhapatapchuk@gmail.com>
2024-03-09 23:59:52 +0100

    Styled icons in skill section

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Icon} from "../../../../components/icon/Icon";
 import styled from "styled-components";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
+import {theme} from "../../../../styles/Theme";
 
 
 type SkillPropsType = {
@@ -11,24 +13,55 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
-            <Icon iconId={props.iconId}></Icon>
-            <SkillTitle>{props.title}</SkillTitle>
-            <SkillText>{props.description}</SkillText>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
+                    <Icon iconId={props.iconId}></Icon>
+                </IconWrapper>
+                <SkillTitle>{props.title}</SkillTitle>
+                <SkillText>{props.description}</SkillText>
+            </FlexWrapper>
         </StyledSkill>
     );
 };
 
 const StyledSkill = styled.div`
-    background-color: rgba(255, 255, 255, 0.37);
-    width: 30%;
-    margin: 10px;
-    padding: 10px 10px 0 10px;
+    width: 380px;
+    padding: 60px 40px 20px 62px;
 `
 
 const SkillTitle = styled.h3`
-
+    margin-top: 70px;
+    margin-bottom: 15px;
+    letter-spacing: 1px;
+    font-size: 16px;
+    line-height: 16px;
+    font-weight: 700;
+    font-family: 'Josefin Sans', sans-serif;
 `
 
 const SkillText = styled.p`
+    text-align: center;
+    letter-spacing: 1px;
+    font-size: 14px;
+    line-height: 21px;
+    font-weight: 400;
+`
 
+const IconWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: "";
+        display: inline-block;
+        background-color: ${theme.colors.primaryBg};
+        width: 80px;
+        height: 80px;
+        transform: rotate(45deg) translate(-50%, -50%);
+                
+        position: absolute;
+        z-index: -1;
+        left: 50%;
+        top: 50%;
+    }
 `
