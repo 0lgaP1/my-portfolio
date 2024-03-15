305fb27ea86b96babd6ba0fe108492b5ce41e84e
0lgaP1 <volhapatapchuk@gmail.com>
2024-03-15 12:34:15 +0100

    Styled contact form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,23 +9,23 @@ export const Button = styled.button`
     width: 170px;
     height: 32px;
     z-index: 0;
-    
+
     &:hover {
         &::before {
-        height: 100%;
-        width: 100%;
+            height: 100%;
+            width: 100%;
+        }
     }
-    }
-    
+
     position: relative;
-    
+
     &::before {
         content: "";
         display: inline-block;
         height: 10px;
         width: 50%;
         background-color: ${theme.colors.accent};
-        
+
         position: absolute;
         bottom: 0;
         left: 50%;
--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,35 +2,68 @@ import React from 'react';
 import {SectionTitle} from "../../../components/SectionTitle";
 import styled from "styled-components";
 import {Button} from "../../../components/Button";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-                <Field placeholder={"Your name"}/>
-                <Field placeholder={"Enter a subject"}/>
-                <Field placeholder="Enter your message" as={"textarea"} />
-                <Button type={"submit"}>Send your message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact</SectionTitle>
+                <StyledForm>
+                    <Field placeholder={"Your name"}/>
+                    <Field placeholder={"Enter a subject"}/>
+                    <Field placeholder="Enter your message" as={"textarea"} />
+                    <Button type={"submit"}>Send your message</Button>
+                </StyledForm>
+            </Container>
         </StyledContacts>
     );
 };
 
 const StyledContacts = styled.section`
-    
+        
 `
 
 const StyledForm = styled.form`
-    max-width: 500px;
+    max-width: 540px;
     width: 100%;
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    align-items: center;
+    gap: 16px;
     margin: 0 auto;
+    
+    textarea {
+        resize: none;
+        height: 155px;
+    }
+        
+    ${Button} {
+        //width: inherit;
+        display: block;
+    }
 `
 
 const Field = styled.input`
+    background-color: ${theme.colors.secondaryBg};
+    border: 1px solid ${theme.colors.border};
+    width: 100%;
+    padding: 7px 15px;
+    
+    font-family: "Popins", sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: 0.05em;
+    
+    color: ${theme.colors.font};
+    
+    &::placeholder {
+        color: ${theme.colors.placeholderColor}
+    }
 
+    &:focus-visible {
+        outline: 1px solid ${theme.colors.border};
+    }
 `
