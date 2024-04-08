1dbc7a9de9dec731a1eeaf1d957a92543f0f3ecf
0lgaP1 <volhapatapchuk@gmail.com>
2024-04-08 19:43:12 +0200

    Scroll animation for skill section (React awesome reveal)

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
 import {IconWrapper} from "../../../../components/IconWrapper";
 import {theme} from "../../../../styles/Theme";
+import { Fade } from "react-awesome-reveal";
 
 
 type SkillPropsType = {
@@ -15,11 +16,13 @@ export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
             <FlexWrapper direction={"column"} align={"center"}>
-                <IconWrapper>
-                    <Icon iconId={props.iconId}></Icon>
-                </IconWrapper>
-                <SkillTitle>{props.title}</SkillTitle>
-                <SkillText>{props.description}</SkillText>
+                <Fade cascade={true} damping={0.2}>
+                    <IconWrapper>
+                        <Icon iconId={props.iconId}></Icon>
+                    </IconWrapper>
+                    <SkillTitle>{props.title}</SkillTitle>
+                    <SkillText>{props.description}</SkillText>
+                </Fade>
             </FlexWrapper>
         </StyledSkill>
     );
@@ -29,7 +32,7 @@ const StyledSkill = styled.div`
     width: 330px;
     flex-grow: 1;
     padding: 42px 20px 40px;
-    
+
     @media ${theme.media.mobile} {
         padding: 62px 0 62px;
     }
