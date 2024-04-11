9845050cfdf857ba5b656da6121a59e86490070c
0lgaP1 <volhapatapchuk@gmail.com>
2024-04-11 12:10:13 +0200

    Map for skill section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -5,24 +5,52 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
 import {Container} from "../../../components/Container";
 
+const skillData = [
+    {
+        iconId: "codeSvg",
+        title: "HTML5",
+        description: "It's a markup language used for structuring and presenting hypertext documents on the World Wide Web",
+    },
+    {
+        iconId: "css",
+        title: "CSS3",
+        description: "It's a style sheet language used for specifying the presentation and styling of a document written in a markup language such as HTML or XML",
+    },
+    {
+        iconId: "react",
+        title: "React",
+        description: "It is a free and open-source front-end JavaScript library for building user interfaces based on components.",
+    },
+    {
+        iconId: "typescript",
+        title: "TypeScript",
+        description: "It is a free and open-source high-level programming language developed by Microsoft that adds static typing with optional type annotations to JavaScript.",
+    },
+    {
+        iconId: "styledComponents",
+        title: "Styled Components",
+        description: "It's a styling library by which JavaScript is used to style components",
+    },
+    {
+        iconId: "webdesign",
+        title: "Figma",
+        description: "It is a collaborative web application for interface design, with additional offline features enabled by desktop applications for macOS and Windows.",
+    }
+]
 export const Skills = () => {
     return (
         <StyledSkills id={"skills"}>
             <Container>
                 <SectionTitle>My skills</SectionTitle>
                 <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-                    <Skill iconId={"codeSvg"} title={"HTML5"}
-                           description={"It's a markup language used for structuring and presenting hypertext documents on the World Wide Web"}/>
-                    <Skill iconId={"css"} title={"CSS3"}
-                           description={"It's a style sheet language used for specifying the presentation and styling of a document written in a markup language such as HTML or XML"}/>
-                    <Skill iconId={"react"} title={"React"}
-                           description={"It is a free and open-source front-end JavaScript library for building user interfaces based on components."}/>
-                    <Skill iconId={"typescript"} title={"TypeScript"}
-                           description={"It is a free and open-source high-level programming language developed by Microsoft that adds static typing with optional type annotations to JavaScript."}/>
-                    <Skill iconId={"styledComponents"} title={"Styled Components"}
-                           description={"It's a styling library by which JavaScript is used to style components"}/>
-                    <Skill iconId={"webdesign"} title={"Figma"}
-                           description={"It is a collaborative web application for interface design, with additional offline features enabled by desktop applications for macOS and Windows."}/>
+
+                    {skillData.map((s, index)=>{
+                        return <Skill iconId={s.iconId} key={index}
+                                      title={s.title}
+                                      description={s.description}
+
+                        />
+                    })}
                 </FlexWrapper>
             </Container>
         </StyledSkills>
